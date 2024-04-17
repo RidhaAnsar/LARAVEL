63eb286963d8c869b7f84f2c6d8a534583465eb6
Ridha Ansar <140969987+RidhaAnsar@users.noreply.github.com>
2024-04-17 19:11:47 +0530

    storage folder

---
+++ storage/framework/views/ec4ca93d3ada696495449424d6e5c631cfbb3d2e.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" >
+  </head>
+  <body>
+    <div class="container">
+    <form action="<?php echo e(route('login.post')); ?>" method="post">
+       <?php echo csrf_field(); ?>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Email address</label>
+          <input type="email" class="form-control" name="email" >
+        </div>
+
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+        </div>
+       
+         
+   
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" ></script>
+  </body>
+</html><?php /**PATH F:\pro\resources\views/login.blade.php ENDPATH**/ ?>
